b5ba0895e1b20aa457b9a1fe78a047da7d83029f
starman360 <anmol.modur@gmail.com>
2014-11-16 08:57:25 -0500

    put graph in footer

--- index.php
+++ index.php
@@ -31,67 +31,6 @@ require($ASSET_PATH."/header.inc");
 		    </div>
 		</div>
 	</div>
-	<script>
-			document.getElementById("get-day").innerHTML = "28";
-			$(function () {
-			    $('#graph').highcharts({
-			        chart: {
-			            zoomType: 'x'
-			        },
-			        title: {
-			            text: 'Amount of Cigaretts Smoked'
-			        },
-			        subtitle: {
-			            text: document.ontouchstart === undefined ?
-			                    'Click and drag in the plot area to zoom in' :
-			                    'Pinch the chart to zoom in'
-			        },
-			        xAxis: {
-			            type: 'datetime',
-			            minRange: 1 * 24 * 3600000 // one day
-			        },
-			        yAxis: {
-			            title: {
-			                text: 'Cigaretts'
-			            }
-			        },
-			        legend: {
-			            enabled: false
-			        },
-			        plotOptions: {
-			            area: {
-			                fillColor: {
-			                    linearGradient: { x1: 0, y1: 0, x2: 0, y2: 1},
-			                    stops: [
-			                        [0, Highcharts.getOptions().colors[0]],
-			                        [1, Highcharts.Color(Highcharts.getOptions().colors[0]).setOpacity(0).get('rgba')]
-			                    ]
-			                },
-			                marker: {
-			                    radius: 2
-			                },
-			                lineWidth: 1,
-			                states: {
-			                    hover: {
-			                        lineWidth: 1
-			                    }
-			                },
-			                threshold: null
-			            }
-			        },
-					series: [{
-		           		type: 'area',
-		            	name: 'USD to EUR',
-		            	pointInterval: 24 * 3600 * 1000,
-		            	pointStart: Date.UTC(2006, 0, 1),
-		            	data: [18,19,18,17,17,17,16,15,15,15,14,14,14,16,17,15,14,14,13,13,13,12,12,12,12,11,11,10]
-		            }]
-				});
-			});
-	</script>
-	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>	
-	<script src="http://code.highcharts.com/highcharts.js"></script>
-	<script src="../module/exporting.js"></script>
 
 <?php
 require($ASSET_PATH."/footer.inc");
