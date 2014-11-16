fb5bc4c04033f83188caf93bdcfccec327f94bc5
starman360 <anmol.modur@gmail.com>
2014-11-16 09:22:45 -0500

    trying stuff

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ require($ASSET_PATH."/header.inc");
 	<div class="body-container">	
 		<div class="wrapper">
 			<div class="days-left">
-				<p>DAY <span id="get-day"></span></p>
+				<p>DAY <span id="get-day">28</span></p>
 			</div>
 			<div class="main-view">
 				<ul id="myTab" class="nav nav-tabs" role="tablist">
@@ -24,7 +24,7 @@ require($ASSET_PATH."/header.inc");
 		     		</div>
 		        	<div role="tabpanel" class="tab-pane fade" id="progress" aria-labelledBy="progress-tab">
 		        		<div class="page-header"><h1>your progress</h1></div>
-		        		<div style="max-width:100%"><div id="graph"></div></div>
+		        		<div id="graph"></div>
 		        		<p>Food truck fixie locavore, accusamus mcsweeney's marfa nulla single-origin coffee squid. Exercitation +1 labore velit, blog sartorial PBR leggings next level wes anderson artisan four loko farm-to-table craft beer twee. Qui photo booth letterpress, commodo enim craft beer mlkshk aliquip jean shorts ullamco ad vinyl cillum PBR. Homo nostrud organic, assumenda labore aesthetic magna delectus mollit. Keytar helvetica VHS salvia yr, vero magna velit sapiente labore stumptown. Vegan fanny pack odio cillum wes anderson 8-bit, sustainable jean shorts beard ut DIY ethical culpa terry richardson biodiesel. Art party scenester stumptown, tumblr butcher vero sint qui sapiente accusamus tattooed echo park.</p>
 		       		</div>
 		    	</div>
