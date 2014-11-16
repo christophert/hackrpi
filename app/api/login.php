30bf13729e52c383c3053ec442b959d9e09f9b6e
officer <officer@enterprise.devices.christran.in>
2014-11-16 02:57:05 -0500

    update login and add api things

---
+++ app/api/login.php
@@ -0,0 +1,20 @@
+<?php
+//login api
+$FUNCPATH = $_SERVER['DOCUMENT_ROOT']."/assets/php/func.inc";
+require($FUNCPATH);
+
+if(empty($_POST['email']) || empty($_POST['password'])) {
+	die("<p>General Error</p>");
+}
+else {
+	session_start();
+	//not secure at all
+	$_SESSION['firstName'] = "John";
+	$_SESSION['lastName'] = "Smith";
+	$_SESSION['userId'] = 1;
+	if(empty($_POST['redir']))
+		header('Location: /');
+	else
+		header('Location /'.urlencode($_POST['redir']));
+}
+?>
--- app/login.php
+++ app/login.php
@@ -1,6 +1,18 @@
 <?php
 //login.php
-$page = 0;
+$page = 99;
 $ASSET_PATH = $_SERVER['DOCUMENT_ROOT']."/app/assets/php";
 require($ASSET_PATH."/header.inc");
+?>
+		<div class="container">
+			<form class="signin" id="signin" role="form" action="/api/login" method="post">
+				<h2 class="heading">Sign in</h2>
+				<input type="email" class="form-control" placeholder="john@smith.com" id="email" name="email" autofocus="autofocus" required>
+				<input type="password" class="form-control" placeholder="password" id="password" name="password" required>
+				<?php if(!empty($_GET['redir'])) { ?><input type="hidden" name="redir" value="<?php echo $_GET['redir']; ?>" /> <?php } ?>
+				<button class="login-button" type="submit">Sign In</button>
+			</form>
+		</div>
+<?php
+require($ASSET_PATH."/footer.inc");
 ?>
