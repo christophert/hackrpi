4f5091ac709015a1ccddd3c686e08bf73d3f3e1c
officer <officer@enterprise.devices.christran.in>
2014-11-16 15:13:59 -0500

    license

---
+++ license.php
@@ -0,0 +1,13 @@
+<?php
+//license.php
+$ASSET_PATH = $_SERVER['DOCUMENT_ROOT']."/assets/php";
+$page = 0;
+require($ASSET_PATH."/header.inc");
+?>
+		<div class="container">
+			<div class="page-header"><h1>NicoQuit License</h1></div>
+			<a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/4.0/"><img alt="Creative Commons License" style="border-width:0" src="https://i.creativecommons.org/l/by-nc-sa/4.0/88x31.png" /></a><br /><span xmlns:dct="http://purl.org/dc/terms/" property="dct:title">NicoQuit</span> by <span xmlns:cc="http://creativecommons.org/ns#" property="cc:attributionName">TranIT</span> is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/4.0/">Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License</a>.
+		</div>
+<?php
+require($ASSET_PATH."/footer.inc");
+?>
