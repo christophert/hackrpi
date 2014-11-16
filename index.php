da6dcefe6471c35d8666ed7c19f3f8d03335dd70
officer <officer@enterprise.devices.christran.in>
2014-11-16 07:37:27 -0500

    goal and profile

--- index.php
+++ index.php
@@ -11,17 +11,19 @@ require($ASSET_PATH."/header.inc");
 			<div class="main-view">
 				<ul id="myTab" class="nav nav-tabs" role="tablist">
 		      		<li role="presentation" class="active">
-		      			<a href="#home" id="home-tab" role="tab" data-toggle="tab" aria-controls="home" aria-expanded="true">Profile</a>
+		      			<a href="#profile" id="profile-tab" role="tab" data-toggle="tab" aria-controls="profile" aria-expanded="true">Profile</a>
 		      		</li>
 		      		<li role="presentation">
-		      			<a href="#profile" role="tab" id="profile-tab" data-toggle="tab" aria-controls="profile">Profile</a>
+		      			<a href="#progress" role="tab" id="progress-tab" data-toggle="tab" aria-controls="progress">Goal</a>
 		      		</li>
 		   		</ul>
 		    	<div id="myTabContent" class="tab-content">
-		      		<div role="tabpanel" class="tab-pane fade in active" id="home" aria-labelledBy="home-tab">
+		      		<div role="tabpanel" class="tab-pane fade in active" id="profile" aria-labelledBy="profile-tab">
+		      			<div class="page-header"><h1>your profile</h1></div>
 		        		<p>Raw denim you probably haven't heard of them jean shorts Austin. Nesciunt tofu stumptown aliqua, retro synth master cleanse. Mustache cliche tempor, williamsburg carles vegan helvetica. Reprehenderit butcher retro keffiyeh dreamcatcher synth. Cosby sweater eu banh mi, qui irure terry richardson ex squid. Aliquip placeat salvia cillum iphone. Seitan aliquip quis cardigan american apparel, butcher voluptate nisi qui.</p>
 		     		</div>
-		        	<div role="tabpanel" class="tab-pane fade" id="profile" aria-labelledBy="profile-tab">
+		        	<div role="tabpanel" class="tab-pane fade" id="progress" aria-labelledBy="progress-tab">
+		        		<div class="page-header"><h1>your progress</h1></div>
 		        		<p>Food truck fixie locavore, accusamus mcsweeney's marfa nulla single-origin coffee squid. Exercitation +1 labore velit, blog sartorial PBR leggings next level wes anderson artisan four loko farm-to-table craft beer twee. Qui photo booth letterpress, commodo enim craft beer mlkshk aliquip jean shorts ullamco ad vinyl cillum PBR. Homo nostrud organic, assumenda labore aesthetic magna delectus mollit. Keytar helvetica VHS salvia yr, vero magna velit sapiente labore stumptown. Vegan fanny pack odio cillum wes anderson 8-bit, sustainable jean shorts beard ut DIY ethical culpa terry richardson biodiesel. Art party scenester stumptown, tumblr butcher vero sint qui sapiente accusamus tattooed echo park.</p>
 		       		</div>
 		    	</div>
