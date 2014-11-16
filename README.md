521cda1a4667abb8c6a14139626e24024e7b109c
starman360 <anmol.modur@gmail.com>
2014-11-16 03:20:37 -0500

    Started building columns
    1% complete

--- README.md
+++
@@ -1,4 +0,0 @@
-hackrpi
-=======
-
-WOOOOOOOO
--- app/index.php
+++ app/index.php
@@ -3,8 +3,22 @@ $ASSET_PATH = $_SERVER['DOCUMENT_ROOT']."/app/assets/php";
 $page = 1;
 require($ASSET_PATH."/header.inc");
 ?>
-	<div class="body-container">
+	<div class="body-container">	
 
 <?php
 require($ASSET_PATH."/footer.inc");
 ?>
+		<!DOCTYPE html>
+		<html lang="en">
+		<head>
+			<meta charset="UTF-8">
+		</head>
+		<body>
+			<div class="container">
+				::before
+				<div class="row">
+					::before
+					<div class="col-xs-3"></div>
+				</div>
+		</body>
+		</html>
